3a5c05f3f9418dbfe245d30a5d7b44f16b7b294d
NSSimpleApps <ns.poluyanov.sergey@gmail.com>
2018-10-07 00:45:21 +0700

    Bumpe to swift42

--- WebAuthForm/AppDelegate.swift
+++ WebAuthForm/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     // MARK: - `UIApplicationDelegate` conformance
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         self.window = UIWindow(frame: UIScreen.main.bounds)
         self.window?.rootViewController = RootViewController.rootViewController
